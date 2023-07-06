f0007de6ed7f594917dcc689b3676d56a5ae835a
Ahsan Ahmed <ahsan@Ahsanserver.local>
2023-07-06 08:51:49 -0700

    Documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,4 +25,19 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+html_static_path = ['images']
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#7C4DFF",
+        "color-brand-content": "#7C4DFF",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#7C4DFF",
+        "color-brand-content": "#7C4DFF",
+    },
+    "light_logo": "l.png",
+    "dark_logo": "d.png",
+
+    "sidebar_hide_name": True
+}
+html_favicon = "images/favicon.png"
