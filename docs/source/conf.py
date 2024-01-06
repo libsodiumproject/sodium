2dc19a686a191c413f5146d4dd895819cf1669e8
Ahsan Ahmed <85203848+zykron1@users.noreply.github.com>
2024-01-05 17:55:23 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['images']
 html_theme_options = {
     "light_css_variables": {
